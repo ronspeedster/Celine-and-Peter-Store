377a697e31d418cc8a8d546fa7f172408da4f4ed
roniespeedster <ronieB03@gmail.com>
2020-08-08 13:13:47 +0800

    Update print_transaction.php

--- print_transaction.php
+++ print_transaction.php
@@ -18,7 +18,7 @@ $balance = $newTransaction['amount_paid'] - $newTransaction['total_amount'];
 
 $getTransactionLists = mysqli_query($mysqli, "SELECT * FROM transaction_lists WHERE transaction_id = '$id' ");
 ?>
-<title>View Transaction - Celine & Peter Store</title>
+<title>Print Transaction - Celine & Peter Store <?php echo $newTransaction['transaction_date'].'-'.$newTransaction['full_name']; ?></title>
 <!-- Content Wrapper -->
 <div id="content-wrapper" class="d-flex flex-column">
 
